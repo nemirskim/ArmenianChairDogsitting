529d735cca3017fea7797a4ff9fe8b811185c14d
lena falina <aternetas@gmail.com>
2022-06-25 13:22:29 +0300

    add ClientsController and endpoints (w/o realization yet)

---
+++ ArmenianChairDogsitting.API/Controllers/ClientsController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ArmenianChairDogsitting.API.Controllers
+{
+    public class ClientsController : Controller
+    {
+        [HttpPost]
+        public void AddNewClient()
+        {
+            
+        }
+
+        [HttpGet("{id}")]
+        public Client GetClientById(int id)
+        {
+            return Client;
+        }
+
+        [HttpGet]
+        public List<Client> GetAllClients()
+        {
+            return List<Client>;
+        }
+
+        [HttpPut("{id}")]
+        public void UpdateClientById(int id)
+        {
+
+        }
+
+        [HttpDelete]
+        public void RemoveClient(int id)
+        {
+
+        }
+
+        [HttpPatch("{id}")]
+        public DeactivateClientById(int id)
+        {
+
+        }
+
+
+
+    }
+}
