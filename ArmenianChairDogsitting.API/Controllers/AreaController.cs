59a8c1cd213addd84a29d0ad367f01df1ee48131
Timur <2001rich04@gmail.com>
2022-06-27 18:30:54 +0300

    Add AreaController

---
+++ ArmenianChairDogsitting.API/Controllers/AreaController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ArmenianChairDogsitting.API.Models;
+
+namespace ArmenianChairDogsitting.API.Controllers
+{
+    public class AreaController : Controller
+    {
+        [HttpPost]
+        public void Add()
+        {
+
+        }
+
+        [HttpGet("{id}")]
+        public Area GetById(int id)
+        {
+            return new Area();
+        }
+
+        [HttpGet]
+        public List<Area> GetAll()
+        {
+            return new List<Area>();
+        }
+
+        [HttpPut("{id}")]
+        public void UpdateById(int id)
+        {
+
+        }
+
+        [HttpDelete("{id}")]
+        public void DeleteById(int id)
+        {
+
+        }
+    }
+}
---
+++ ArmenianChairDogsitting.API/Models/Area.cs
@@ -0,0 +1,9 @@
+﻿namespace ArmenianChairDogsitting.API.Models
+{
+    public class Area
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Square { get; set; }
+    }
+}
