7d23f31121c94acf9b38fd822df3cb5020f3fbb2
Ilya Bozhkov <ilia.il.27@mail.ru>
2022-06-29 05:11:52 +0300

    Filled out the class AuthController & add class AuthOptions

--- ArmenianChairDogsitting.API/Controllers/AuthController.cs
+++ ArmenianChairDogsitting.API/Controllers/AuthController.cs
@@ -1,11 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using ArmenianChairDogsitting.API.Infrastructure;
+using ArmenianChairDogsitting.API.Models;
 
 namespace ArmenianChairDogsitting.API.Controllers;
 
 [ApiController]
-
 [Route("[controller]")]
 public class AuthController : Controller
 {
+    [HttpPost]
+    public string Login([FromBody] UserLoginRequest request)
+    {
+        if (request == default || request.Email == default) return string.Empty;
+        var roleClaim = new Claim(ClaimTypes.Role, (request.Email == "q@qq.qq" ? Role.Admin : Role.Regular).ToString());
+        var claims = new List<Claim> { new Claim(ClaimTypes.Name, request.Email), roleClaim };
+        var jwt = new JwtSecurityToken(
+                issuer: AuthOptions.ISSUER,
+                audience: AuthOptions.AUDIENCE,
+                claims: claims,
+                expires: DateTime.UtcNow.Add(TimeSpan.FromMinutes(2)), // время действия 2 минуты
+                signingCredentials: new SigningCredentials(AuthOptions.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
 
+        return new JwtSecurityTokenHandler().WriteToken(jwt);
+    }
 }
