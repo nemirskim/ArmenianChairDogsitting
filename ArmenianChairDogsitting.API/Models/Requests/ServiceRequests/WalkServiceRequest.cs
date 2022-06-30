a43bd816f4d5b0d8e6456b20f7e44778c8544b74
Timur <2001rich04@gmail.com>
2022-07-01 01:14:33 +0300

    Add WalkServiceRequest

---
+++ ArmenianChairDogsitting.API/Models/Requests/ServiceRequests/WalkServiceRequest.cs
@@ -0,0 +1,15 @@
+﻿namespace ArmenianChairDogsitting.API.Models
+{
+    public class WalkServiceRequest
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal CostPerWalk { get; set; }
+        public decimal CostPerDog { get; set; }
+        public decimal StartCost { get; set; }
+        public int WalkQuantity { get; set; }
+        public int DogQuantity { get; set; }
+        public List<string> Promocodes { get; set; }
+        public bool IsTrial { get; set; }
+    }
+}
