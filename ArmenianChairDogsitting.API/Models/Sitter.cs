2338fa75cbfaadb6f30426131b39c644d9d638a7
Ilya Bozhkov <ilia.il.27@mail.ru>
2022-07-01 16:55:11 +0300

    Add Response & Request models for class Sitter

--- ArmenianChairDogsitting.API/Models/Sitter.cs
+++ ArmenianChairDogsitting.API/Models/Sitter.cs
@@ -2,5 +2,15 @@
 {
     public class Sitter
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public int Age { get; set; }
+        public int Experience {  get; set; }
+        public string Sex { get; set; }
+
+        //......
     }
 }
