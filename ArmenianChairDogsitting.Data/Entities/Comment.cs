5e20faee970e51af83f6274985b135262c6831c3
Timur <2001rich04@gmail.com>
2022-07-05 23:36:04 +0300

    Add CommentEntity

---
+++ ArmenianChairDogsitting.Data/Entities/Comment.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace ArmenianChairDogsitting.Data.Entities
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public int ClientId { get; set; }
+        public string Title { get; set; }
+        public Order Order { get; set; }
+        public DateTime TimeCreated { get; set; }
+
+    }
+}
