5109215e224fc1e93c0157b34a5cbbab213968d8
Ilya <ilia.il.27@mail.ru>
2022-07-05 19:06:17 +0300

    Minor fix

--- ArmenianChairDogsitting.Data/Entities/Sitter.cs
+++ ArmenianChairDogsitting.Data/Entities/Sitter.cs
@@ -4,9 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ArmenianChairDogsitting.Data.Entities
+namespace ArmenianChairDogsitting.Data.Entities;
+
+public class Sitter
 {
-    internal class Sitter
-    {
-    }
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string LastName { get; set; }
+    public string Phone { get; set; }
+    public string Email { get; set; }
+    public int Age { get; set; }
+    public int Experience { get; set; }
+    public Sex Sex { get; set; }
+    public string Description { get; set; }
+    public Dictionary<Service, decimal> PriceCatalog { get; set; }
 }
