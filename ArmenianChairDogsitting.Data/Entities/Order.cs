e6b88c70f9e7e2d04d6679d3a4e967c8ddbc75a7
Timur <2001rich04@gmail.com>
2022-07-05 18:26:22 +0300

    Add OrderEntity

---
+++ ArmenianChairDogsitting.Data/Entities/Order.cs
@@ -0,0 +1,17 @@
+﻿namespace ArmenianChairDogsitting.Data.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public int ClientId { get; set; }
+        public List<Animal> Animals { get; set; }
+        public string Status { get; set; }
+        public int? DayQuantity { get; set; }
+        public int? WalkQuantity { get; set; }
+        public int? WalkPerDayQuantity { get; set; }
+        public int? HourQuantity { get; set; }
+        public int? VisitQuantity { get; set; }
+        public bool? IsTrial { get; set; }
+    }
+}
