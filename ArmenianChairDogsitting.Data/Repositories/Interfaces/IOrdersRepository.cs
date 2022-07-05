aff79e153002266e24a9adfd950dcb1167e7d5cd
Timur <2001rich04@gmail.com>
2022-07-05 18:48:12 +0300

    Create Interface IOrderRepository

---
+++ ArmenianChairDogsitting.Data/Repositories/Interfaces/IOrdersRepository.cs
@@ -0,0 +1,13 @@
+﻿using ArmenianChairDogsitting.Data.Entities;
+using ArmenianChairDogsitting.Data.Enums;
+
+namespace ArmenianChairDogsitting.Data.Repositories.Interfaces
+{
+    public interface IOrdersRepository
+    {
+        int AddOrder(Order order);
+        int UpdateOrderStatus(Status status, int orderId);
+        Order GetOrderById(int orderId);
+        List<Order> GetAllOrders();
+    }
+}
