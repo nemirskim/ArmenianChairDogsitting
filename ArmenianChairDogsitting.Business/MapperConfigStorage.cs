b8bed436d84c2539c1615ce92d6c0f7f5b2fdc3f
Timur <2001rich04@gmail.com>
2022-07-09 16:27:46 +0300

    Add mapperConfig

--- ArmenianChairDogsitting.Business/MapperConfigStorage.cs
+++ ArmenianChairDogsitting.Business/MapperConfigStorage.cs
@@ -1,9 +1,26 @@
 ﻿using AutoMapper;
 using ArmenianChairDogsitting.Data.Entities;
+using ArmenianChairDogsitting.Business.Models;
 
 namespace ArmenianChairDogsitting.Business;
 
 public class MapperConfigStorage
 {
+    private static Mapper _instance;
 
+    public static Mapper GetInstance()
+    {
+        if (_instance == null)
+            InitializeInstance();
+        return _instance;
+    }
+
+    private static void InitializeInstance()
+    {
+        _instance = new Mapper(new MapperConfiguration(cfg =>
+        {
+            cfg.CreateMap<Comment, CommentModel>();
+            cfg.CreateMap<CommentModel, Comment>();
+        }));
+    }
 }
--- ArmenianChairDogsitting.Business/Models/CommentModel.cs
+++ ArmenianChairDogsitting.Business/Models/CommentModel.cs
@@ -2,4 +2,10 @@
 
 public class CommentModel
 {
+    public int Id { get; set; }
+    public Client Client { get; set; }
+    public string Title { get; set; }
+    public Order Order { get; set; }
+    public DateTime TimeCreated { get; set; }
+    public bool IsDeleted { get; set; }
 }
