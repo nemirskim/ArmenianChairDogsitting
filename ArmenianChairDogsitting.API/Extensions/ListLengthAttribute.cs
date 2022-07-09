39a7807742eaa6bae6b271d54b2f84f10533de2d
Timur <2001rich04@gmail.com>
2022-07-10 01:45:24 +0300

    Add ListLengthAttribute

---
+++ ArmenianChairDogsitting.API/Extensions/ListLengthAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.ComponentModel.DataAnnotations;
+
+namespace ArmenianChairDogsitting.API.Extensions;
+
+public class ListLengthAttribute : ValidationAttribute
+{
+    public int Minimum { get; set; }
+    public int Maximum { get; set; }
+
+    public ListLengthAttribute(int min = int.MinValue, int max = int.MaxValue)
+    {
+        Minimum = min;
+        Maximum = max;
+    }
+
+    public override bool IsValid(object value)
+    {
+        var list = value as IList;
+        if (list is null
+            || list.Count < Minimum 
+            || list.Count > Maximum)
+        {
+            return false;
+        }
+        return true;
+    }
+}
