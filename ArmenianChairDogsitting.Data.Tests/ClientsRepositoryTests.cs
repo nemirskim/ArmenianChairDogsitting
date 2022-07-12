26817583c1c2d88416406f78290ea601d74e7fea
lena falina <aternetas@gmail.com>
2022-07-12 17:26:29 +0300

    fix CommentsRepositoryTests & OrdersRepositoryTests

--- ArmenianChairDogsitting.Data.Tests/ClientsRepositoryTests.cs
+++ ArmenianChairDogsitting.Data.Tests/ClientsRepositoryTests.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ArmenianChairDogsitting.Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,4 +10,32 @@ namespace ArmenianChairDogsitting.Data.Tests;
 
 public class ClientsRepositoryTests
 {
+    private DbContextOptions<ArmenianChairDogsittingContext> _dbContextOptions;
+    private ArmenianChairDogsittingContext _context;
+    private ClientsRepository _sut;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _dbContextOptions = new DbContextOptionsBuilder<ArmenianChairDogsittingContext>()
+            .UseInMemoryDatabase(databaseName: $"TestDb")
+            .Options;
+
+        _context = new ArmenianChairDogsittingContext(_dbContextOptions);
+        _context.Database.EnsureDeleted();
+        _sut = new ClientsRepository(_context);
+
+        _context.SaveChanges();
+    }
+
+    [Test]
+    public void RemoveClient_WhenCorrectIdIsPassed_ThenSoftDeleteApplied()
+    {
+        //given
+
+        //when
+
+        //then
+    }
+
 }
