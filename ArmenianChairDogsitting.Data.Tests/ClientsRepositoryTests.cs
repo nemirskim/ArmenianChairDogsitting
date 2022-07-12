e743b6e59839602068a65d025e6a09eb09210e0a
lena falina <aternetas@gmail.com>
2022-07-12 19:09:45 +0300

    add GetClientByIdTest

--- ArmenianChairDogsitting.Data.Tests/ClientsRepositoryTests.cs
+++ ArmenianChairDogsitting.Data.Tests/ClientsRepositoryTests.cs
@@ -38,7 +38,45 @@ public class ClientsRepositoryTests
             //Orders = new Order() { Id = 1, Type = new DailySitting() },       
         });
 
+        _context.Clients.Add(new Client()
+        {
+            Id = 2,
+            Name = "Konstantin",
+            LastName = "Zasnul",
+            Dogs = new()
+                { new() { Id = 65, Name = "Persik" }},
+            IsDeleted = true
+            //Orders = new Order() { Id = 1, Type = new DailySitting() },       
+        });
+
         _context.SaveChanges();
+    }
+
+    [TestCaseSource(typeof(ClientsTestCaseSource))]
+    public void AddClientTest_WhenNewClientIsCorrect_ThenShouldAddExpectedClient(Client newClient, Client expected)
+    {
+        //given       
+        var actual = _context.Clients.FirstOrDefault(c => c.Id == newClient.Id);
+
+        //when
+        _sut.AddClient(newClient);
+
+        //then
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestCaseSource(typeof(ClientsTestCaseSource))]
+    public void GetClientByIdTest_WhenIdIsCorrect_ShouldReturnExpectedClient(Client newClient, Client expected)
+    {
+        //given
+        _context.Clients.Add(newClient);
+        _context.SaveChanges();
+
+        //when
+        _sut.GetClientById(5);
+
+        //then
+        Assert.AreEqual(expected.Id, newClient.Id);
     }
 
     [Test]
@@ -53,6 +91,20 @@ public class ClientsRepositoryTests
 
         //then
         Assert.True(client!.IsDeleted);
+    }
+
+    [Test]
+    public void RestoreClientTest_WhenCorrectIdIsPassed_ThenRestoreProfileApplied()
+    {
+        //given
+        var id = 2;
+        var client = _context.Clients.FirstOrDefault(c => c.Id == id);
+
+        //when
+        _sut.RemoveOrRestoreClient(id);
+
+        //then
+        Assert.False(client!.IsDeleted);
     }
 
 
