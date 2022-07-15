04f5b21d34b66baeafee9de52620686433e2875b
lena falina <aternetas@gmail.com>
2022-07-15 18:28:28 +0300

    add methods in ClientsControllerTests

--- ArmenianChairDogsitting.API.Tests/ClientsControllerTests.cs
+++ ArmenianChairDogsitting.API.Tests/ClientsControllerTests.cs
@@ -38,4 +38,84 @@ public class ClientsControllerTests
         Assert.AreEqual(StatusCodes.Status201Created, actualResult.StatusCode);
         Assert.AreEqual(id, actualResult.Value);
     }
+
+    [Test]
+    public void GetClientByIdTest_WhenCorrectIdPassed_ThenReturnExpectedClient()
+    {
+        //given
+        var client = new ClientAllInfoResponse();
+        var id = 1;
+
+        //when
+        var actual = _sut.GetClientById(id);
+
+        //then
+        var actualResult = actual.Result as ObjectResult;
+
+        Assert.AreEqual(StatusCodes.Status200OK, actualResult.StatusCode);
+        Assert.AreEqual(client.GetType(), actualResult.Value.GetType());
+    }
+
+    [Test]
+    public void GetAllClientsTest_WhenRequestPassed_ThenShouldReturnClients()
+    {
+        //given
+        var expected = new ClientAllInfoResponse();
+
+        //when
+        var actual = _sut.GetAllClients();
+
+        //then
+        var actualResult = actual.Result as ObjectResult;
+
+        Assert.AreEqual(StatusCodes.Status200OK, actualResult.StatusCode);
+        Assert.AreEqual(expected.GetType(), actualResult.Value.GetType());
+    }
+
+    [Test]
+    public void UpdateClientTest_WhenCorrectIdPassed_ThenClientProfileUpdated()
+    {
+        //given
+        var client = new ClientUpdateRequest() { Name = "Marina" };
+        var id = 1;
+
+        //when
+        var actual = _sut.UpdateClient(client, id);
+
+        //then
+        var actualResult = actual as NoContentResult;
+
+        Assert.AreEqual(StatusCodes.Status204NoContent, actualResult.StatusCode);
+    }
+
+    [Test]
+    public void RemoveClientTest_WhenCorrectIdPassed_ThenSoftDeleteApplied()
+    {
+        //given
+        var id = 1;
+
+        //when
+        var actual = _sut.RemoveClient(id);
+
+        //then
+        var actualResult = actual as NoContentResult;
+
+        Assert.AreEqual(StatusCodes.Status204NoContent, actualResult.StatusCode);
+    }
+
+    [Test]
+    public void RestoreClientTest_WhenCorrectIdIsPassed_ThenRestoreClientProfile()
+    {
+        //given
+        var id = 1;
+
+        //when
+        var actual = _sut.RestoreClient(id);
+
+        //then
+        var actualResult = actual as NoContentResult;
+
+        Assert.AreEqual(StatusCodes.Status204NoContent, actualResult.StatusCode);
+    }
+
 }
