304140978a0e1e4953a7af26a277589987d86156
Timur <2001rich04@gmail.com>
2022-07-29 17:12:13 +0300

    fix names

--- ArmenianChairDogsitting.API/Models/Requests/OrderRequests/AbstractOrderRequest.cs
+++ ArmenianChairDogsitting.API/Models/Requests/OrderRequests/AbstractOrderRequest.cs
@@ -12,6 +12,7 @@ public abstract class AbstractOrderRequest
     [Required]
     public int SitterId { get; set; }
     [ListLength(1, 4, ErrorMessage = ApiErrorMessage.DogQuantityError)]
-    public List<int> AnimalsId { get; set; }
+    public List<int> AnimalIds { get; set; }
     public ServiceEnum Type { get; set; }
+    public Status Status { get; set; }
 }
