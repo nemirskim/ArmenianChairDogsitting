d08737126e446909e01a27d436366c8bcb4c0456
Maxim Bespalov <i.maksim_01@mail.ru>
2022-08-16 20:40:35 +0300

    add all animals repo tests

--- ArmenianChairDogsitting.Data.Tests/AnimalsRepositoryTests.cs
+++ ArmenianChairDogsitting.Data.Tests/AnimalsRepositoryTests.cs
@@ -37,17 +37,115 @@ public class AnimalsRepositoryTests
         
         _context.Animals.Add(new Animal()
         {
-            Id = 1,
+            Id = 2,
             Name = "Warik",
             Breed = "Korgi",
             Age = 3,
             Size = SizeOfAnimal.SmallerThanTenKg,
             RecommendationsForCare = "mnogo kormit'",
-            ClientId = 3,
+            ClientId = 2,
             Orders = new List<Order>(),
             IsDeleted = false
         });
 
         _context.SaveChanges();
+    }
+
+    [Test]
+    public void AddAnimal_WhenNewAnimalIsCorrect_ThenShouldAddExpectedAnimal()
+    {
+        //given 
+        var dog = new Animal()
+        {
+            Id = 3,
+            Name = "Kysaka",
+            Breed = "Siba iny",
+            Age = 4,
+            Size = SizeOfAnimal.SmallerThanTenKg,
+            RecommendationsForCare = "lubit'",
+            ClientId = 2,
+            Orders = new List<Order>(),
+            IsDeleted = false
+        };
+
+        //when 
+        _sut.AddAnimal(dog);
+
+        //then 
+        var expected = _context.Animals.FirstOrDefault(a => a.Id == dog.Id);
+        Assert.AreEqual(expected.Id, dog.Id);
+    }
+
+    [Test]
+    public void GetAnimalById_WhenIdIsCorrect_ShouldReturnExpectedAnimal()
+    {
+        //given in setup 
+
+        //when 
+        var dog = _sut.GetAnimalById(2);
+
+        //then 
+        Assert.AreEqual(2, dog.Id);
+        Assert.NotNull(dog);
+    }
+
+    [Test]
+    public void GetAllAnimalsByClient_ShouldReturnAllOfExpectedAnimals()
+    {
+        //given 
+        var expectedCountOfDogs = 2;
+
+        //when 
+        var dog = _sut.GetAllAnimalsByClient(2);
+
+        //then 
+        Assert.AreEqual(expectedCountOfDogs, dog.Count);
+    }
+
+    [Test]
+    public void UpdateAnimal_WhenCorrectIdIsPassed_ThenUpdateAnimalApplied()
+    {
+        //given 
+        var id = 2;
+        var actual = _context.Animals.FirstOrDefault(a => a.Id == id);
+        actual.Breed = "Wpic";
+        _context.SaveChanges();
+
+        //when
+        _sut.UpdateAnimal(actual, id);
+
+        //then 
+        Assert.AreEqual(id, actual.Id);
+        Assert.AreEqual("Wpic", actual.Breed);
+    }
+
+    [Test]
+    public void RemoveAnimal_CorrectIdIsPassed_ThenSoftDeleteApplied()
+    {
+        //given 
+        var id = 1;
+        var dog = _context.Animals.FirstOrDefault(a => a.Id == id);
+        dog.IsDeleted = true;
+
+        //when 
+        _sut.RemoveOrRestoreAnimal(dog);
+
+        //then 
+        Assert.True(dog.IsDeleted);
+    }
+
+    [Test]
+    public void RestoreAnimal_WhenCorrectIdIsPassed_ThenRestoreApplied()
+    {
+        //given 
+        var id = 2;
+        var dog = _context.Animals.FirstOrDefault(a => a.Id == id);
+        dog.IsDeleted = false;
+
+        //when 
+        _sut.RemoveOrRestoreAnimal(dog);
+
+        //then 
+        Assert.False(dog.IsDeleted);
     }
 }
